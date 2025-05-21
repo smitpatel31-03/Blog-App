557063219607b86539961d23baee381729bc4c00
smit <smit@gmail.com>
2025-05-21 10:48:25 +0530

    fix: update authentication prop for Home component in router configuration

--- Frontend/Admin/src/main.jsx
+++ Frontend/Admin/src/main.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
       {
         path:"/",
         element:(
-            <Home authentication={false}/>
+            <Home authentication={true}/>
         )
       },
       {
