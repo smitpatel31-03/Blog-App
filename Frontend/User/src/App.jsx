db29fec54515954b02cf2f57da3380b6331ba65d
smit <smit@gmail.com>
2025-05-21 17:17:15 +0530

    fix: update email subscription logic to set a default value after successful subscription

--- Frontend/User/src/App.jsx
+++ Frontend/User/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       const sendMail = await Services.addEmail({ email });
       if (sendMail) {
         toast.success("Subscribed Successfully");
-        setEmail("");
+        setEmail("a");
       } else {
         toast.error("Something Went Wrong While Adding Email");
       }
