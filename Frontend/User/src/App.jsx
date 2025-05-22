23c658420eca399c55293468040a4be57cebd853
smit <smit@gmail.com>
2025-05-22 10:00:37 +0530

    fix: adjust positioning of pencil2 and comaps SVG images for improved layout

--- Frontend/User/src/App.jsx
+++ Frontend/User/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
       <img src="/svg/cap.svg" className="absolute top-4 left-10 w-20 sm:w-75 sm:top-4 sm:left-80" />
       <img src="/svg/pencil1.svg" className="absolute top-4 left-35 w-25 sm:w-70 sm:top-10 sm:left-175" />
       <img src="/svg/notebook.svg" className="absolute z-1 top-2 left-60 w-30 sm:w-80 sm:top-2 sm:left-250" />
-      <img src="/svg/pencil2.svg" className="absolute top-5 left-75 w-25 sm:w-70 sm:top-10 sm:left-310" />
+      <img src="/svg/pencil2.svg" className="absolute top-15 left-75 w-25 sm:w-70 sm:top-10 sm:left-310" />
       <img src="/svg/kids1.svg" className="absolute top-30 w-30 sm:w-75 sm:top-30 sm:left-5" />
       <img src="/svg/pencil3.svg" className="absolute top-100 left-6 w-25 sm:w-40 sm:left-85 sm:top-110" />
 
@@ -45,7 +45,7 @@ function App() {
         <img src="/svg/heading.svg" className="flex justify-center w-50 sm:w-150" />
 
         {/* First input box */}
-        <div className="mt-6 w-70 sm:max-w-md">
+        <div className="mt-6 w-50 sm:max-w-md">
           <div className="flex mt-[-60px] items-center justify-center px-4 py-2 sm:mt-[-100px]">
             <i className="fas fa-envelope text-gray-400 mr-2" />
             <input
@@ -73,7 +73,7 @@ function App() {
       <img src="/svg/kids2.svg" className="absolute bottom-30 left-[-60px] w-50 sm:w-130 sm:left-[-150px]" />
       <img src="/svg/hands.svg" className="absolute bottom-40 left-25 w-25 sm:w-50 sm:left-70" />
       <img src="/svg/brush.svg" className="absolute bottom-30 left-45 w-25 sm:w-50 sm:left-150" />
-      <img src="/svg/comaps.svg" className="absolute bottom-40 z-1 left-40 w-25 sm:w-50 sm:left-210" />
+      <img src="/svg/comaps.svg" className="absolute bottom-50 z-1 left-50 w-25 sm:w-50 sm:left-210" />
       <img src="/svg/kids3.svg" className="absolute bottom-30 right-[-50px] w-50 sm:w-130 sm:right-[-110px]" />
 
       <ToastContainer />
